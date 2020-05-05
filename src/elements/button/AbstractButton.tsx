9401d2494f5b39646dd7f31e8fa635f9335982c7
OleksiiKachan <okachan@nobul.com>
2020-05-04 22:14:20 -0400

    feat(elements): redo abstract button

--- src/elements/button/AbstractButton.tsx
+++ src/elements/button/AbstractButton.tsx
@@ -1,33 +1,76 @@
-import styled from 'styled-components';
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 
-export default styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  margin: 0;
-  border: none;
-  cursor: pointer;
-  font-size: 20px;
-  font-weight: 700;
-  line-height: 24px;
-  min-height: 50px;
-  max-width: 160px;
-  width: 100%;
-  padding: 10px;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  font-family: 'Open Sans', sans-serif;
-  white-space: nowrap;
+import './styles.scss';
 
-  &:focus,
-  &:active {
-    outline: 2px dotted #6f777b !important;
-    outline-offset: 3px;
+type PropsType = {
+  children: ReactNode;
+  as?: ReactNode | string | Function;
+  className?: string;
+  disabled?: boolean;
+  href?: string;
+  type?: 'button' | 'reset' | 'submit';
+  tabIndex?: number;
+  role?: string;
+};
+
+const AbstractButton = React.forwardRef(
+  (
+    {
+      children,
+      as,
+      className,
+      disabled = false,
+      href,
+      type = 'button',
+      tabIndex = 0,
+      ...other
+    }: PropsType,
+    ref
+  ) => {
+    const buttonClasses = classNames(className, {
+      [`abstract-button`]: true,
+      [`abstract-button--disabled`]: disabled,
+      className,
+    });
+
+    const commonProps = {
+      tabIndex,
+      className: buttonClasses,
+      ref,
+    };
+
+    let component: any = 'button';
+    let otherProps: object = {
+      disabled,
+      type,
+    };
+
+    const anchorProps = {
+      href,
+    };
+
+    if (as) {
+      component = as;
+      otherProps = {
+        ...otherProps,
+        ...anchorProps,
+      };
+    } else if (href && !disabled) {
+      component = 'a';
+      otherProps = { ...anchorProps };
+    }
+
+    return React.createElement(
+      component,
+      {
+        ...other,
+        ...commonProps,
+        ...otherProps,
+      },
+      children
+    );
   }
+);
 
-  &:disabled {
-    cursor: not-allowed;
-    pointer-events: none;
-  }
-`;
+export default AbstractButton;
