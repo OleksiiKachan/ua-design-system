217b9d82745f13b0e666d82aed67bd69be6f07bc
OleksiiKachan <okachan@nobul.com>
2020-05-24 20:28:08 -0400

    build(storybook): more updates on storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,120 +1,41 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const rtlcss = require('rtlcss');
-const customProperties = require('postcss-custom-properties');
-
-const useExternalCss = false;
-const useRtl = false;
-
-module.exports = ({ config, mode }) => {
-  config.devtool = 'source-map';
-  config.optimization = {
-    ...config.optimization,
-    minimizer: [
-      new TerserPlugin({
-        sourceMap: true,
-        terserOptions: {
-          mangle: false,
-        },
-      }),
-    ],
-  };
-
-  config.module.rules.push({
-    test: /.stories\.jsx?$/,
-    loaders: [
-      {
-        loader: require.resolve('@storybook/source-loader'),
-        options: {
-          prettierConfig: {
-            parser: 'babylon',
-            printWidth: 80,
-            tabWidth: 2,
-            bracketSpacing: true,
-            trailingComma: 'es5',
-            singleQuote: true,
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => {
+              const autoPrefixer = require('autoprefixer')({
+                overrideBrowserslist: ['last 1 version', 'ie >= 11'],
+              });
+              return [
+                customProperties(),
+                autoPrefixer,
+                ...(useRtl ? [rtlcss] : []),
+              ];
+            },
+            sourceMap: true,
           },
         },
-      },
-    ],
-    enforce: 'pre',
-  });
-
-  // config.resolve.extensions.push(['.ts', '.tsx']);
-  config.module.rules.push({ test: /\.tsx?$/, loaders: 'ts-loader' });
-
-  const sassLoader = {
-    loader: 'sass-loader',
-    options: {
-      prependData() {
-        return `
-          $feature-flags: (
-            ui-shell: true,
-            enable-css-custom-properties: true,
-          );
-        `;
-      },
-      sassOptions: {
-        includePaths: [path.resolve(__dirname, '..', 'node_modules')],
-      },
-      sourceMap: true,
+      ],
     },
-  };
-
-  const fastSassLoader = {
-    loader: 'fast-sass-loader',
-    options: {
-      data: `
-        $feature-flags: (
-          ui-shell: true,
-          enable-css-custom-properties: true,
-        );
-      `,
+    {
+      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+      exclude: /\/favicon.ico$/,
+      loader: 'file',
     },
-  };
-
-  config.module.rules.push({
-    test: /\.scss$/,
-    sideEffects: true,
-    use: [
-      {
-        loader: useExternalCss ? MiniCssExtractPlugin.loader : 'style-loader',
-      },
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 2,
-          sourceMap: true,
-        },
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          plugins: () => {
-            const autoPrefixer = require('autoprefixer')({
-              overrideBrowserslist: ['last 1 version', 'ie >= 11'],
-            });
-            return [
-              customProperties(),
-              autoPrefixer,
-              ...(useRtl ? [rtlcss] : []),
-            ];
-          },
-          sourceMap: true,
-        },
-      },
-      sassLoader,
-    ],
-  });
-
-  if (useExternalCss) {
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-      })
-    );
-  }
+    { test: /\.tsx?$/, loader: 'ts-loader' },
+    {
+      test: /.stories\.jsx?$/,
+      use: ['@storybook/source-loader'],
+      enforce: 'pre',
+    }
+  );
 
   config.resolve = {
     extensions: ['.ts', '.tsx', '.js'],
--- src/button/stories/index.stories.jsx
+++ src/button/stories/index.stories.jsx
@@ -1,95 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, select, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 import Button from '../';
-
-const icons = {
-  None: 'None',
-  // 'Add (Add16 from `@carbon/icons-react`)': 'Add16',
-  // 'Search (Search16 from `@carbon/icons-react`)': 'Search16',
-};
-
-// const iconMap = {
-//   iconAddSolid,
-//   iconSearch,
-//   Add16,
-//   Search16,
-// };
-
-const kinds = {
-  'Blue button (blue)': 'blue',
-};
 
 const props = {
   regular: () => {
-    // const iconToUse = iconMap[select('Icon (icon)', icons, 'none')];
     return {
       className: 'some-class',
-      // kind: select('Button kind (kind)', kinds, 'primary'),
       disabled: boolean('Disabled (disabled)', false),
-      // renderIcon: !iconToUse || iconToUse.svgData ? undefined : iconToUse,
       onClick: action('onClick'),
       onFocus: action('onFocus'),
     };
   },
-  // iconOnly: () => {
-  //   const iconToUse = iconMap[select('Icon (icon)', icons, 'Add16')];
-  //   return {
-  //     className: 'some-class',
-  //     kind: select(
-  //       'Button kind (kind)',
-  //       {
-  //         'Primary button (primary)': 'primary',
-  //         'Secondary button (secondary)': 'secondary',
-  //         'Tertiary button (tertiary)': 'tertiary',
-  //         'Ghost button (ghost)': 'ghost',
-  //       },
-  //       'primary'
-  //     ),
-  //     disabled: boolean('Disabled (disabled)', false),
-  //     size: select('Button size (size)', sizes, 'default'),
-  //     renderIcon: !iconToUse || iconToUse.svgData ? undefined : iconToUse,
-  //     iconDescription: text(
-  //       'Icon description (iconDescription)',
-  //       'Button icon'
-  //     ),
-  //     tooltipPosition: select(
-  //       'Tooltip position (tooltipPosition)',
-  //       ['top', 'right', 'bottom', 'left'],
-  //       'bottom'
-  //     ),
-  //     tooltipAlignment: select(
-  //       'Tooltip alignment (tooltipAlignment)',
-  //       ['start', 'center', 'end'],
-  //       'center'
-  //     ),
-  //     onClick: action('onClick'),
-  //     onFocus: action('onFocus'),
-  //   };
-  // },
-  // set: () => {
-  //   const iconToUse = iconMap[select('Icon (icon)', icons, 'none')];
-  //   return {
-  //     className: 'some-class',
-  //     disabled: boolean('Disabled (disabled)', false),
-  //     size: select('Button size (size)', sizes, 'default'),
-  //     renderIcon: !iconToUse || iconToUse.svgData ? undefined : iconToUse,
-  //     iconDescription: text(
-  //       'Icon description (iconDescription)',
-  //       'Button icon'
-  //     ),
-  //     onClick: action('onClick'),
-  //     onFocus: action('onFocus'),
-  //   };
-  // },
 };
-
-// const CustomLink = ({ children, href, ...other }) => (
-//   <a href={href} {...other}>
-//     {children}
-//   </a>
-// );
 
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
@@ -183,46 +107,3 @@ storiesOf('Buttons', module)
       },
     }
   );
-// .add('Icon-only buttons', () => <Button {...props.iconOnly()} hasIconOnly />)
-// .add(
-//   'Sets of Buttons',
-//   () => {
-//     const setProps = props.set();
-//     return (
-//       <div className={`${prefix}--btn-set`}>
-//         <Button kind="secondary" {...setProps}>
-//           Secondary button
-//         </Button>
-//         <Button kind="primary" {...setProps}>
-//           Primary button
-//         </Button>
-//       </div>
-//     );
-//   },
-//   {
-//     info: {
-//       text: `
-//         When an action required by the user has more than one option, always use a a negative action button (secondary) paired with a positive action button (primary) in that order. Negative action buttons will be on the left. Positive action buttons should be on the right. When these two types buttons are paired in the correct order, they will automatically space themselves apart.
-//       `,
-//     },
-//   }
-// )
-// .add(
-//   'skeleton',
-//   () => (
-//     <div>
-//       <ButtonSkeleton />
-//       &nbsp;
-//       <ButtonSkeleton href="#" />
-//       &nbsp;
-//       <ButtonSkeleton size="small" />
-//     </div>
-//   ),
-//   {
-//     info: {
-//       text: `
-//         Placeholder skeleton state to use when content is loading.
-//       `,
-//     },
-//   }
-// );
